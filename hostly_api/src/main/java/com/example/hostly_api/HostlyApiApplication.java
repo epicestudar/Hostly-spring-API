e3f0ce241fb564d84d918763cc2238dfd6eae76c
epicestudar <viniciusftza@gmail.com>
2024-10-03 13:34:05 -0300

    criando projeto

---
+++ hostly_api/src/main/java/com/example/hostly_api/HostlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostlyApiApplication.class, args);
+	}
+
+}
